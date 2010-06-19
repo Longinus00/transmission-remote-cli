b8ab1dda742b73a62eb4e7fd780c0e96678c1386
Dmitry Nedospasov <dmitry@nedos.net>
2010-06-19 17:32:46 +0200

    Change the colors so that there is more contrast (easier to read)

--- transmission-remote-cli.py
+++ transmission-remote-cli.py
@@ -608,14 +608,14 @@ class Interface:
             curses.start_color()
             curses.init_pair(1,  curses.COLOR_BLACK,    curses.COLOR_BLUE)  # download rate
             curses.init_pair(2,  curses.COLOR_BLACK,    curses.COLOR_RED)   # upload rate
-            curses.init_pair(3,  curses.COLOR_BLUE,     curses.COLOR_BLACK) # unfinished progress
-            curses.init_pair(4,  curses.COLOR_GREEN,    curses.COLOR_BLACK) # finished progress
+            curses.init_pair(3,  curses.COLOR_BLUE,     curses.COLOR_WHITE) # unfinished progress
+            curses.init_pair(4,  curses.COLOR_GREEN,    curses.COLOR_WHITE) # finished progress
             curses.init_pair(5,  curses.COLOR_BLACK,    curses.COLOR_WHITE) # eta/ratio
             curses.init_pair(6,  curses.COLOR_CYAN,     curses.COLOR_BLACK) # idle progress
             curses.init_pair(7,  curses.COLOR_MAGENTA,  curses.COLOR_BLACK) # verifying
             curses.init_pair(8,  curses.COLOR_WHITE,    curses.COLOR_BLACK) # button
             curses.init_pair(9,  curses.COLOR_BLACK,    curses.COLOR_WHITE) # focused button
-            curses.init_pair(10, curses.COLOR_WHITE,     curses.COLOR_RED)   # stats filter
+            curses.init_pair(10, curses.COLOR_RED,      curses.COLOR_WHITE) # stats filter
         except:
             pass
 
